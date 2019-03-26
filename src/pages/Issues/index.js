b2197526dc9424588a0ec4f6d963390810ae2f02
Thais Moraes <murici.thais@gmail.com>
2019-03-26 09:45:02 -0300

    Initial navigation structure

---
+++ src/pages/Issues/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Text, View } from 'react-native';
+
+// import styles from './styles';
+
+const Issues = () => (
+  <View>
+    <Text>Issues</Text>
+  </View>
+);
+
+export default Issues;
---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+
+import Issues from '~/pages/Issues';
+import Repositories from '~/pages/Repositories';
+
+const Routes = createAppContainer(
+  createStackNavigator({
+    Repositories,
+    Issues,
+  }),
+);
+
+export default Routes;
